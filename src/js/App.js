3307d788a5ee5a62be350e7607b3210568128205
yinhaiying <yinhaiying@58.com>
2020-04-29 22:45:37 +0800

    add:抽离公共的类

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,35 @@
-// 公共的东西
+// 所有页面公共的东西
+import "../css/common.scss"
 
+import {IndexModel} from "../models/index"
 class App {
-  constructor(){
-
+  constructor($,options){
+    this.$app = $('<div id = "app"></div>');
+    this.swiper = options.swiper;
+    this.phone = options.phone;
+    this.field = options.field;
+    this.cache = null;
+    this.init();
+  }
+  async init(){
+    await  this.getDatas();
+    this.render();
+  }
+  async getDatas(){
+    const indexModel = new IndexModel();
+    await indexModel.getDatas({
+      swiper:this.swiper,
+      phone:this.phone,
+      field:this.field
+    }).then((res) => {
+      this.cache = {
+        phoneDatas:res.phone_data,
+        fieldDatas:res.field_data,
+        swiperDatas:res.swiper_data
+      }
+      console.log('cache')
+      console.log(this.cache)
+    })
   }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,20 @@
-import "../css/common.scss"
-import {IndexModel} from "../models/index"
-import {tplReplace,  trimSpaces,getUrlQuery,throttle} from '../utils/tools.js';
 
+import {tplReplace,  trimSpaces,getUrlQuery,throttle} from '../utils/tools.js';
+import {App} from './App'
 import { Header } from "../components/header";
 
-class Index {
-  constructor(){
-    this.$app = $('<div id = "app"></div>');
-    this.init();
-  }
-
-  init(){
-    this.getDatas();
-    this.render();
+// Index页面继承公共的类的功能
+class Index extends App{
+  constructor($){
+    super($,{
+      swiper:true,
+      phone:true,
+      field:true
+    })
   }
   render(){
     new Header(this.$app);
     $('body').prepend(this.$app);
-  }
-  getDatas(){
-    const indexModel = new IndexModel();
-    indexModel.getDatas({swiper:true,phone:true,field:true})
-    .then((res) => {
-      console.log('getDatas')
-      console.log(res)
-    })
   }
 }
 
