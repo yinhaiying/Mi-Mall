6f50996779e86ced27e70f9e16c9aa36557cdb2f
yinhaiying <18840842571@163.com>
2020-05-02 11:48:08 +0800

    build

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const htmlWebpackPlugin = require("html-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 const miniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
     index: path.resolve(__dirname, "./src/js/Index.js"),
     list: path.resolve(__dirname, "./src/js/List.js"),
